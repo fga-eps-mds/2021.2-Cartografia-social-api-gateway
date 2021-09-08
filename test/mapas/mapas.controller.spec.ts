493f0bf7cfdc8e341ed7a8a656d17c9083e04678
Elias Bernardo <ebmm01@gmail.com>
2021-09-07 21:23:15 -0300

    Adding unit tests

--- test/mapas/mapas.controller.spec.ts
+++ test/mapas/mapas.controller.spec.ts
@@ -1,8 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Observable } from 'rxjs';
 import { MapasController } from '../../src/mapas/mapas.controller';
 
 describe('MapasController', () => {
   let controller: MapasController;
+
+  const customModule = async (fn: any) => {
+    return await Test.createTestingModule({
+      providers: [
+        {
+          provide: 'MAPA_SERVICE',
+          useValue: {
+            send: fn,
+          },
+        },
+      ],
+      controllers: [MapasController],
+    }).compile();
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,5 +37,23 @@ describe('MapasController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Create a point ', async () => {
+    const id = { id: '123' };
+    const module = await customModule(
+      jest.fn(() => new Observable((sub) => sub.next(id))),
+    );
+
+    controller = module.get<MapasController>(MapasController);
+
+    expect(
+      await controller.createPoint({
+        title: 'teste',
+        description: 'teste',
+        latitude: 0,
+        longitude: 0,
+      }),
+    ).toBe(id);
   });
 });
