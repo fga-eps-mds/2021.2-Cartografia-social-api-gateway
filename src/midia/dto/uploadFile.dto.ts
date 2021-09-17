0dc99d477156b62fe17e2c6a40269e5c75655f85
Guilherme Guy <guilhermeguy349@gmail.com>
2021-09-17 09:25:34 -0300

    [#6] feat - Add uploadFile DTO
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>
    Co-authored-by: Elias Bernardo <ebmm01@gmail.com>

---
+++ src/midia/dto/uploadFile.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+
+export class UploadFileDto {
+
+    constructor(file: Express.Multer.File) {
+
+        this.file = file;
+        this.bufferB64 = file.buffer.toString('base64');
+
+        // clean the file bufffer, because we are sending it as base64
+        // the default implementation messes up the data
+        this.file.buffer = null;
+    }
+
+    file: Express.Multer.File;
+    bufferB64: string;
+
+}
