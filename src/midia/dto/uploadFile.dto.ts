85858d698f66c34e7b7a4e4592a6df1483934b7e
Guilherme Guy <guilhermeguy349@gmail.com>
2021-09-22 15:02:23 -0300

    [#6] style - Fix linter errors
    Co-authored-by Leonardo Barreiros <leossb36@gmail.com>

--- src/midia/dto/uploadFile.dto.ts
+++ src/midia/dto/uploadFile.dto.ts
@@ -1,19 +1,13 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-
 export class UploadFileDto {
+  constructor(file: Express.Multer.File) {
+    this.file = file;
+    this.bufferB64 = file.buffer.toString('base64');
 
-    constructor(file: Express.Multer.File) {
+    // clean the file bufffer, because we are sending it as base64
+    // the default implementation messes up the data
+    this.file.buffer = null;
+  }
 
-        this.file = file;
-        this.bufferB64 = file.buffer.toString('base64');
-
-        // clean the file bufffer, because we are sending it as base64
-        // the default implementation messes up the data
-        this.file.buffer = null;
-    }
-
-    file: Express.Multer.File;
-    bufferB64: string;
-
+  file: Express.Multer.File;
+  bufferB64: string;
 }
