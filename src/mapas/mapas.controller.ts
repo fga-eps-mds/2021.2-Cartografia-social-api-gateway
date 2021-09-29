ed33766f2bb8fc3ab08fa6542a0c2f14f1fa7c0a
Leonardo Barreiros <leossb36@gmail.com>
2021-09-29 19:57:48 -0300

    [#34] feat - add endpoints relations from area & points
    Signed-off-by: Leonardo Barreiros <leossb36@gmail.com>
    Co-authored-by: Guilherme Guy <guilhermeguy349@gmail.com>

--- src/mapas/mapas.controller.ts
+++ src/mapas/mapas.controller.ts
@@ -6,6 +6,7 @@ import {
   Inject,
   Param,
   Post,
+  Put,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ApiTags } from '@nestjs/swagger';
@@ -15,6 +16,7 @@ import { IdResponseModel } from '../responseModels/id';
 import { AreaDto } from './dto/area.dto';
 import { CreateAreaDto } from './dto/create-area.dto';
 import { CreatePointDto } from './dto/createPoint.dto';
+import { MediaRelationDto } from './dto/media-relation.dto';
 
 @ApiTags('maps')
 @Controller('maps')
@@ -38,6 +40,35 @@ export class MapasController {
     return mapaResponse;
   }
 
+  @Get('point')
+  public async getPoint(@Body() id: string): Promise<CreatePointDto> {
+    const mapaResponse = await firstValueFrom<CreatePointDto>(
+      this.mapaServiceClient.send('getPoint', id),
+    );
+
+    return mapaResponse;
+  }
+
+  @Put('point')
+  public async updatePoint(
+    @Body() createPointDto: CreatePointDto,
+  ): Promise<IdResponseModel> {
+    const mapaResponse = await firstValueFrom<IdResponseModel>(
+      this.mapaServiceClient.send('updatePoint', createPointDto),
+    );
+
+    return mapaResponse;
+  }
+
+  @Delete('point')
+  public async deletePoint(@Body('id') id: string): Promise<boolean> {
+    await firstValueFrom<CreatePointDto>(
+      this.mapaServiceClient.send('deletePoint', id),
+    );
+
+    return true;
+  }
+
   @Post('area')
   public async createArea(
     @Body() createAreaDto: CreateAreaDto,
@@ -45,6 +76,19 @@ export class MapasController {
     const mapaResponse = await firstValueFrom<IdResponseModel>(
       this.mapaServiceClient
         .send('createArea', createAreaDto)
+        .pipe(timeout(TEN_SECONDS)),
+    );
+
+    return mapaResponse;
+  }
+
+  @Put('area')
+  public async updateArea(
+    @Body() createAreaDto: CreateAreaDto,
+  ): Promise<IdResponseModel> {
+    const mapaResponse = await firstValueFrom<IdResponseModel>(
+      this.mapaServiceClient
+        .send('updateArea', createAreaDto)
         .pipe(timeout(TEN_SECONDS)),
     );
 
@@ -62,6 +106,30 @@ export class MapasController {
   public async deleteArea(@Param('id') id: string): Promise<boolean> {
     await firstValueFrom<AreaDto>(
       this.mapaServiceClient.send('deleteArea', id).pipe(timeout(TEN_SECONDS)),
+    );
+    return true;
+  }
+
+  @Post('addMediaToPoint')
+  public async addMediaToPoint(
+    @Body() mediaRelationDto: MediaRelationDto,
+  ): Promise<boolean> {
+    await firstValueFrom<MediaRelationDto>(
+      this.mapaServiceClient
+        .send('addMediaToPoint', mediaRelationDto)
+        .pipe(timeout(TEN_SECONDS)),
+    );
+    return true;
+  }
+
+  @Delete('removeMediaFromPoint')
+  public async removeMediaFromPoint(
+    @Body() mediaRelationDto: MediaRelationDto,
+  ): Promise<boolean> {
+    await firstValueFrom<MediaRelationDto>(
+      this.mapaServiceClient
+        .send('removeMediaFromPoint', mediaRelationDto)
+        .pipe(timeout(TEN_SECONDS)),
     );
     return true;
   }
