202a6b7d68cf812c092a1fae2616dedb6b9e22b6
Marcos N <marcosnery.comp@gmail.com>
2022-04-14 21:29:23 -0300

    feat: adding new fields to non validated user

--- src/users/dto/create-non-validated-user.dto.ts
+++ src/users/dto/create-non-validated-user.dto.ts
@@ -2,9 +2,9 @@ export class CreateNonValidatedUserDto {
     name: string;
     email: string;
     cellPhone: string;
+    type: string;
     password?: string;
-    role: string;
-    position: string;
-    affiliation: string;
-    community: string;
+    role?: string;
+    affiliation?: string;
+    community?: string;
 }
